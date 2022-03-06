d6ce29120d295761975a8e52cb51707767c12b23
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-06 19:17:43 -0300

    Delete unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
+import { LoginActivate } from './loginactivate.guard';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [LoginActivate],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
