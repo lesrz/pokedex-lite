9a40af78071653001fa799b168a7906f9500b3ad
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-07 02:23:15 -0300

    Create pokedex service

--- src/app/pages/home/pokedex.service.ts
+++ src/app/pages/home/pokedex.service.ts
@@ -1,9 +1,37 @@
+import { AuthService } from '@app/auth.service';
+import { Pokemon } from './../../shared/models/pokemon';
+import { map, Observable, BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokedexService {
+  public pokemonToEdit!: Pokemon;
+  constructor(private http: HttpClient, private authService: AuthService) {}
+  private userId = this.authService.userValue?.userId;
+  // Gets all Pokémon data for current user
+  getAllPokemon(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>(
+      `${environment.apiUrl}/pokemon?userId=${this.userId}`
+    );
+  }
 
-  constructor() { }
+  updatePokemon(pokemon: Pokemon): Observable<any> {
+    return this.http.put(`${environment.apiUrl}/pokemon`, pokemon);
+  }
+
+  addPokemon(pokemon: Pokemon): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/pokemon`, pokemon);
+  }
+
+  selectedPokemon(pokemon: Pokemon): void {
+    this.pokemonToEdit = pokemon;
+  }
+
+  getEditablePokemon(pokemon: Pokemon): Pokemon {
+    return this.pokemonToEdit;
+  }
 }
