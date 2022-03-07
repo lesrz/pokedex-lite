b44a6114f80765d3bfdd9fd8f600f8088a7cf612
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-07 02:21:02 -0300

    Add pages to home

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,11 +1,26 @@
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'add',
+    component: AddComponent,
+  },
+  {
+    path: 'edit',
+    component: EditComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
