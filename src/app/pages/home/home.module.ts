2f98cd97b640b3a23158dba749282dcd90a6cecb
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-07 02:22:59 -0300

    Add home module imports

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,17 +1,14 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { EditComponent } from './edit/edit.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, EditComponent, AddComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, ReactiveFormsModule],
 })
-export class HomeModule { }
+export class HomeModule {}
