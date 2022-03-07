9b2889c61b83ea329b9d1094a84744572c3cb3ef
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-07 02:22:44 -0300

    Add home component logic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,10 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Pokemon } from './../../shared/models/pokemon';
+import { PokedexService } from './pokedex.service';
 import { AuthService } from '@app/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  username = this.authService.userValue?.username;
-  constructor(public authService: AuthService) {}
+  public pokedex: Pokemon[] = [];
+  public editPkmn!: Pokemon;
+  constructor(
+    public authService: AuthService,
+    public pokedexSvc: PokedexService,
+    public router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.retrievePokedex();
+  }
 
   onLogout(): void {
     this.authService.logout();
+  }
+
+  retrievePokedex(): void {
+    this.pokedexSvc.getAllPokemon().subscribe({
+      next: (data: Pokemon[]) => {
+        this.pokedex = data;
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log(error.message); //!Remove later
+      },
+    });
+  }
+
+  onAdd(): void {
+    this.router.navigate(['add'], { relativeTo: this.route });
+  }
+
+  onSelect(pokemon: Pokemon): void {
+    this.pokedexSvc.selectedPokemon(pokemon);
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
