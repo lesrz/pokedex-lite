194c81a6a680f3823a635d04213b99d758418290
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-07 14:32:59 -0300

    Add edit functionality

--- src/app/pages/home/edit/edit.component.ts
+++ src/app/pages/home/edit/edit.component.ts
@@ -1,3 +1,5 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PokedexService } from './../pokedex.service';
 import { Component, OnInit } from '@angular/core';
@@ -18,6 +20,54 @@ export class EditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  onSave(form: NgForm): void {
+    // Get form value as variable for modification
+    const formVal = form.value;
+    // Extract values to match interface
+    const abilitiesValues: { description: string; name: string }[] = [
+      {
+        description: formVal.firstAbilityDesc,
+        name: formVal.firstAbilityName,
+      },
+      {
+        description: formVal.secondAbilityDesc,
+        name: formVal.secondAbilityName,
+      },
+    ];
+    // Add empty string for split to work
+    formVal.type += '';
+    const type: string[] = formVal.type.split(/[ ,]+/);
+    // A forEach function won't work due to unordered keys
+    delete formVal.firstAbilityName;
+    delete formVal.firstAbilityDesc;
+    delete formVal.secondAbilityName;
+    delete formVal.secondAbilityDesc;
+    delete formVal.type;
+    // Mientras programaba esta función, accidentalmente
+    // creé un valor null en la base de datos y no hay
+    // manera de eliminarlo desde la API
+    const pokemon: Pokemon = {
+      abilities: [...abilitiesValues],
+      evolutionId: formVal.evolutionId,
+      id: formVal.id,
+      image: formVal.image,
+      lvl: formVal.lvl,
+      name: formVal.name,
+      type: type,
+    };
+    this.pokedexSvc.updatePokemon(pokemon).subscribe({
+      next: (res: Pokemon) => {
+        console.log(res);
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+      complete: () => {
+        this.return();
+      },
+    });
+  }
 
   return(): void {
     this.router.navigate([''], { relativeTo: this.route });
